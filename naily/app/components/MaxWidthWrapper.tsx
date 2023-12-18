f0628da9c00546cd20f097340a4420e67f9a3e72
Nam Tran <nomnom@Nams-Air.vc.shawcable.net>
2023-12-18 01:21:56 -0800

    Initial commit to navigation

---
+++ naily/app/components/MaxWidthWrapper.tsx
@@ -0,0 +1,12 @@
+import { ReactNode } from "react";
+
+const MaxWidthWrapper = ({
+    className,
+    children
+} : {
+    className?: string
+    children: ReactNode
+}) => {
+}
+
+export default MaxWidthWrapper;
